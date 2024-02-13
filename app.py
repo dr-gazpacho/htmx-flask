e97cc61e1ca9a33bbc48138821b6c88e11e869f4
dr-gazpacho <gonzaloandresgazpacho@gmail.com>
2024-02-12 21:22:07 -0600

    set up form to add new entry to db

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_htmx import HTMX, make_response
 from flask_pymongo import PyMongo
 # this project
 from utils.template_decorator import templated
-from utils.products import manage_db
+from utils.products import manage_db, manufacture_product
 
 # initialize flask, htmx
 app=Flask(__name__)
@@ -12,14 +12,14 @@ htmx=HTMX(app)
 # set up database connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/htmx_flask"
 mongo=PyMongo(app)
-# create a few items in the db if its empty
+# create a few items in the db if its empty and clear the cart
 manage_db(mongo) 
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/shelf", methods=["GET", "POST"])
+@app.route("/getShelf", methods=["GET", "POST"])
 @templated("./partials/shelf.html")
 def shelf():
     shelf=mongo.db.inventory.find({})
@@ -28,7 +28,7 @@ def shelf():
         shelf=mongo.db.inventory.find({}).sort(method)
     return dict(shelf=shelf)
 
-@app.route("/add", methods=["POST"])
+@app.route("/addToCart", methods=["POST"])
 @templated("./partials/cart.html")
 def add():
     product=int(request.form.get("product"))
@@ -50,7 +50,7 @@ def add():
         cart=mongo.db.cart.find({})
         return dict(cart=cart)
 
-@app.route("/remove", methods=["DELETE"])
+@app.route("/removeFromCart", methods=["DELETE"])
 @templated("./partials/cart.html")
 def remove():
     product=int(request.form.get("product"))
@@ -60,21 +60,14 @@ def remove():
     cart=mongo.db.cart.find({})
     return dict(cart=cart)
 
-@app.route("/thing", methods=["POST"])
-@templated("./partials/thing.html")
+@app.route("/addToShelf", methods=["POST"])
+@templated("./partials/shelf.html")
 def makeNewThing():
-    name=request.form.get("thing-name")
-    description=request.form.get("thing-description")
-    return dict(name=name, description=description)
-
-# still need to work on this
-    # I need to return SOMETHING to the FE, maybe just remove the clicked element from the DOM and add it to the 'cart'
-    # I think I need to make the color an input, have the button deactivated? then able to submit on color click?
-# @app.route("/addToCart", methods=["POST"])
-# def addToCart():
-#     color=request.args.get('color')
-#     item=request.args.get('henry')
-#     print(color, item)
-#     return '1'
+    name=request.form.get("name")
+    description=request.form.get("description")
+    price=request.form.get("price")
+    manufacture_product(name, description, price, mongo)
+    shelf=mongo.db.inventory.find({})
+    return dict(shelf=shelf)
 
 # app.run(debug=True)
